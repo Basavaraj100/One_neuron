7597f329b4bc31339e19b54da121cc62de6da450
Basavaraj100 <67173421+Basavaraj100@users.noreply.github.com>
2021-09-28 19:35:24 +0530

    file completed

--- and.py
+++ and.py
@@ -1,6 +1,12 @@
 from utils.model import Perceptron
-from utils.all_utils import prefare_data
-
+from utils.all_utils import prefare_data, save_model,save_plot
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib  # for saving model as binary fiel
+from matplotlib.colors import ListedColormap
+import os
+plt.style.use('fivethirtyeight')  # for grapg style
 
 
 
@@ -8,6 +14,12 @@ AND={'x1':[0,1,0,1],
     'x2':[0,0,1,1],
     'y':[0,0,0,1]}
 df=pd.DataFrame(AND)
-df
+
 x,y=prefare_data(df)
-x
+
+
+eta=0.1
+epochs=10
+model=Perceptron(eta,epochs)
+model.fit(x,y)
+_=model.total_loss()
